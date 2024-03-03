358083758af666aca685b4f857417974b1f7ca79
Dijana Pavlovic <dijana@edgedb.com>
2024-03-03 17:34:15 +0100

    Update some styles

--- src/routes/read/[slug]/+page.ts
+++ src/routes/read/[slug]/+page.ts
@@ -1,7 +1,7 @@
 export const load = async ({params}) => {
 	// load the markdown file based on slug
 	const component = await import(
-		`$lib/content/blogs/${params.slug}.svx`
+		`../../../lib/content/blogs/${params.slug}.svx`
 	);
 
 	return {
