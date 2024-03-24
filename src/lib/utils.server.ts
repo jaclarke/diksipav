29d7cfa4cd3118165a5439fbee727f85c65c8881
Dijana Pavlovic <dijana@edgedb.com>
2024-03-24 22:24:09 +0100

    Add post:efficient way to read files in Rust

--- src/lib/utils.server.ts
+++ src/lib/utils.server.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 import matter from 'gray-matter';
-import {read} from '$app/server';
+import { read } from '$app/server';
 
 const blogSchema = z.object({
 	id: z.string(),
@@ -10,7 +10,6 @@ const blogSchema = z.object({
 	tags: z.string()
 });
 
-
 const blogsDir = import.meta.glob('$lib/content/blogs/**/*.svx', {
 	query: 'url',
 	eager: true
@@ -19,14 +18,16 @@ const blogsDir = import.meta.glob('$lib/content/blogs/**/*.svx', {
 const blogs = Object.values(blogsDir);
 
 export async function getAllBlogs() {
-	const reads = blogs.map( async blog =>  {
-		const fileBuffer = await read(blog.default).arrayBuffer();
-		const decoder = new TextDecoder();
-		const str = decoder.decode(fileBuffer);
-		return matter(str).data as z.infer<typeof blogSchema>;	
-	});
+	const reads = await Promise.all(
+		blogs.map(async (blog) => {
+			const fileBuffer = await read(blog.default).arrayBuffer();
+			const decoder = new TextDecoder();
+			const str = decoder.decode(fileBuffer);
+			return matter(str).data as z.infer<typeof blogSchema>;
+		})
+	);
 
 	// todo implement sorting
-	return Promise.all(reads);
-	// return reads.sort((a, b) => b.frontmatter.date - a.frontmatter.date);
+
+	return reads.sort((a, b) => b.date - a.date);
 }
