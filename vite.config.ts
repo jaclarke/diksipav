f3b71e66615a9746756414e222214827a966d849
Dijana Pavlovic <dijana@edgedb.com>
2024-03-25 10:28:17 +0100

    Try some updated styles

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit()
-	],
+	plugins: [sveltekit()],
 	css: {
 		preprocessorOptions: {
 			scss: {
 				additionalData: `
 				  @import '$lib/styles/_mixins.scss';
-					@import '$lib/styles/material-dark.css';
+					@import '$lib/styles/vsc-dark-plus.css';
 
         `
 			}
